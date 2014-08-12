a842f688aa396edb945cf77ef33240ab0c4292a6
Kennethmak <Kenneth.mak@ryerson.ca>
2014-08-12 18:31:06 -0400

    exercise3

--- exercise3.rb
+++ exercise3.rb
@@ -1,7 +1,9 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
+#asks for name then prints the input
 
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 puts "You are #{age} years old"
+#asks for age then prints age as an integer
