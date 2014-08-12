783970cad3808fe5dcef6c76e35ffeb4410a6aa8
Kennethmak <Kenneth.mak@ryerson.ca>
2014-08-12 18:24:49 -0400

    exercise2

---
+++ exercise2.rb
@@ -0,0 +1,16 @@
+tips = 55 * 1.13
+puts "#{tips}"
+#calculates tips for 55 dollars
+
+a = "88"
+b = 2
+puts a.to_i + b
+#converts a into an integer then adds b as a calculation
+
+a = 45628
+b = 7839
+puts "#{a * b}"
+#string interpolation to get value of a * b in one sentence
+
+(true && false) || (false && true) || !(false && false)
+#first two values equal to false while last value is true because of !, false || true = true, therefore this statement is true
