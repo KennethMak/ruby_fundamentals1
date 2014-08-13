8e5d201d3b8faee0e79e1b08f5dff5307c990f6d
Kennethmak <Kenneth.mak@ryerson.ca>
2014-08-13 09:50:38 -0400

    exercise4

--- exercise4.rb
+++ exercise4.rb
@@ -1,11 +1,15 @@
 counter = 1
 until counter > 100
 	if 
-		counter % 3 == 0
+		counter % 3 == 0 && counter % 5 == 0
+		puts "BitMaker"
+	
+	elsif counter % 3 == 0
 		puts "Bit"
-	elsif
-		counter % 5 == 0
+	
+	elsif counter % 5 == 0
 		puts "Maker"
+	
 	else
 		puts counter
 	end
